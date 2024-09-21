cf59dae38379c4b60d22a86d18ee27fba9d4a3fd
Lemonade12 <plus2829@gmail.com>
2024-09-21 21:50:06 +0900

    fix: search rest api url 수정

--- src/routes/stockRouter.ts
+++ src/routes/stockRouter.ts
@@ -3,6 +3,6 @@ const stockController = require('../controllers/stockController');
 
 const router = express.Router();
 
-router.get('/stock/:id', stockController.readCurrentPriceController); // 해당 종목 현재가
+//router.get('/stock/:id', stockController.readCurrentPriceController); // 해당 종목 현재가
 
 module.exports = router;
